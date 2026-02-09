149d03542b0ebc38393a24e0acb9bd9e8e9d352c
kev1n999 <sopratesta999@gmail.com>
2026-02-09 18:03:21 -0300

    add a simple form to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+import Form from "@/src/components/dashboard/form";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1 className="text-center">Dashboard</h1>
+    <div className="flex justify-center min-h-screen min-w-screen mt-10">
+      <div className="grid grid-cols-2">
+        { /* Cycle Tracker */}
+        <div>
+          <h1 className="text-4xl font-extrabold text-violet-300">Calcule seu ciclo aqui!</h1>
+
+          <Form />
+        </div>
+
+        {/* Insights */}
+        <div>
+
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/dashboard/form.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+export default function Form() {
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <form className="flex flex-col gap-2">
+      <label>Primeiro dia da última menstrução:</label>
+      <input type="date" className="rounded-lg border-violet-400"></input>
+
+      <label>Duração do ciclo (dias):</label>
+      <input type="number" min="21" max="35" className="rounded-lg  border-violet-400" required></input>
+
+      <label>Duração da menstruação:</label>
+      <input type="number" min="2" max="8" className="rounded-lg  border-violet-400"></input>
+
+      <button type="submit" className="rounded-lg bg-violet-400 mx-auto mt-5 p-3 min-w-lg text-white cursor-pointer transition-colors duration-300 hover:bg-violet-500">Calcular</button>
+    </form>
+  )
+}
